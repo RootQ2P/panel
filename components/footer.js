a0121abcc3ec1fbfc6869a076fdfa9182d50e6a6
Didid <hinej4@gmail.com>
2022-12-23 23:17:55 +0000

    1

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <div style={{ textAlign: "center", marginTop: 10 }}><a href="https://didid.gg" style={{ color: "grey" }}>&copy; 2022 Didid</a></div>
+    )
+}
